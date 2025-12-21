22e9fcdebb5c2952452b0a8c6e32ff6f5acab30a
iammehedi4201 <iammehedi296@gmail.com>
2025-12-21 10:42:50 +0600

    chore: clean up unused files and modules across the project

--- src/middlewares/common/checkProfileCompletion.ts
+++ src/middlewares/common/checkProfileCompletion.ts
@@ -1,5 +1,4 @@
 import { NextFunction, Request, Response } from "express";
-import { Vendor } from "../../Modules/Vendor/Vendor.model";
 import CatchAsync from "../../Utils/CatchAsync";
 import sendResponse from "../../Utils/SendResponse";
 
@@ -7,27 +6,6 @@ export const checkProfileCompletion = CatchAsync(
   async (req: Request, res: Response, next: NextFunction) => {
     try {
       const userId = req.user.id;
-      const vendor = await Vendor.findById(userId);
-
-      if (!vendor) {
-        return sendResponse(res, {
-          success: false,
-          statusCode: 404,
-          message: "Vendor not found",
-          data: null,
-        });
-      }
-
-      if (!vendor.isProfileComplete) {
-        return sendResponse(res, {
-          success: false,
-          statusCode: 403,
-          message: "Please complete your profile to continue",
-          data: {
-            nextStep: "/profile",
-          },
-        });
-      }
 
       next();
     } catch (error) {
