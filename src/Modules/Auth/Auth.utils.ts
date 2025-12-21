11ffa6f32159eb81ddc1958c0ce8aeb57ce40619
iammehedi4201 <iammehedi296@gmail.com>
2025-12-21 10:42:13 +0600

    refactor: update CreateAccessToken function to use ENV configuration for JWT secrets and expiration

--- src/Modules/Auth/Auth.utils.ts
+++ src/Modules/Auth/Auth.utils.ts
@@ -1,11 +1,16 @@
-import jwt from "jsonwebtoken";
-import config from "../../config";
+// ...existing code...
+import { ENV } from "@/config";
+import * as jwt from "jsonwebtoken";
 import { IJwtPayload } from "../User/User.interface";
 
-const CreateAccessToken = async (payLoad: IJwtPayload) => {
-  return jwt.sign(payLoad, config.jwt_access_token_secret as string, {
-    expiresIn: config.jwt_access_token_expires_in,
-  });
+const CreateAccessToken = async (payLoad: IJwtPayload): Promise<string> => {
+  return jwt.sign(
+    payLoad as jwt.JwtPayload,
+    ENV.JWT_ACCESS_SECRET_KEY as jwt.Secret,
+    {
+      expiresIn: ENV.ACCESS_TOKEN_EXPIERY,
+    } as jwt.SignOptions,
+  );
 };
 
 export default CreateAccessToken;
